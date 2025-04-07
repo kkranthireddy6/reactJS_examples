1147cfb0ea7a41aa304e761e610ffe3896d83d5e
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-04-07 13:38:42 -0500

    Create App.js

---
+++ useCallback/Comonents/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react'
+import ParentComponent from './ParentComponent'
+
+function App() {
+  return (
+    <div>
+      <ParentComponent />
+    </div>
+  )
+}
+
+export default App
