2893d839081424ad8ade7ff9b7d1ca652f0b2c75
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-04-07 13:14:18 -0500

    Create App.js

---
+++ react-CustomHooks-Counter/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ComponentOne from './ComponentOne'
+import ComponentTwo from './ComponentTwo'
+
+function App() {
+  return (
+    <div>
+      <ComponentOne />
+      <ComponentTwo />
+    </div>
+  )
+}
+
+export default App
