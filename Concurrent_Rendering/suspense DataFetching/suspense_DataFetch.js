65091c7ec8389ced46d41a02804027b57c43a543
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-05-11 18:09:10 -0500

    Create suspense_DataFetch.js

---
+++ Concurrent_Rendering/suspense DataFetching/suspense_DataFetch.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect, Suspense, lazy } from "react";
+const profile = lazy(() => import('./Profile'))
+
+export default function App() {
+  return (
+    <div>
+      <h1>Welcome to Concurent Rendering...</h1>
+      <Suspense fallback={<div>Loading Profile...</div>}>
+        <profile />
+      </Suspense>
+    </div>
+  );
+}
+
+
+function Profile() {
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setUser({ name: 'kranti', age: 20 })
+    }, 1000)
+  }, [])
+
+  if (!user) {
+    return <div>Loading user data...</div>
+  }
+
+  return (
+    <>
+      <h1>Profile</h1>
+      <p>Name: {user.name}</p>
+      <p>Age: {user.age}</p>
+    </>
+  )
+}
