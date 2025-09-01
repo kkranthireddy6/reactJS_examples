0196e3e7de98a4c98964cc91238083be0dfe6070
kkranthireddy6 <kkranthireddy6@gmail.com>
2025-08-31 19:44:39 -0500

    Create express_ssr.js

---
+++ expressSSR/express_ssr.js
@@ -0,0 +1,39 @@
+//npm i react express @babel/register @babel/preset-env @babel/preset-react ignore-styles  
+
+//index.js
+require('ignore-styles')
+
+require("@babel/register")({
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    ignore: [/(node_module)/]
+})
+
+require('./server')
+
+//server.js
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+
+import react from 'react'
+import ReactDOMServer from 'react-dom/server'
+import App from '../src/App'
+
+const app = express()
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')))
+
+app.get('/', (req, res, next)=>{
+    fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data)=>{
+        if(err){
+            return res.status(500).send(err)
+        }
+        return res.send(data.replace('<div id="root"></div>', `<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`))
+    })
+})
+
+
+const port = 4000
+app.listen(port, ()=>{
+    console.log('Working 4000')
+})
